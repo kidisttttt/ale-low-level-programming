1bcd291c031f6341331c62d4207483b504c222ba
kidisttttt <111090077+kidisttttt@users.noreply.github.com>
2022-09-21 20:52:28 +0300

    Create 0-strcat.c

---
+++ 0-strcat.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+#include<srting.h>
+int main(){
+char string[50]
+char string[50]
+print("Enter the first string:");
+gets(string1);
+print("Enter the second string:);
+gets(string2);
+strcat(string1,string2);
+print("string concatenation is %s \n",string1);
+return 0;
+}
